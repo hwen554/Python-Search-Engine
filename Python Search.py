0999dfe8c58291cd068c15622b043c0bf034e147
HONGARRACTIVE <78581470+hwen554@users.noreply.github.com>
2021-11-10 16:36:37 +1300

    Update Python Search.py

--- Python Search.py
+++ Python Search.py
@@ -1,7 +1,7 @@
 import translator as translator #import translator
 from bs4 import BeautifulSoup   #import BeautifulSoup
 import requests #import requests
-
+import pyttsx3 #import pyttsx3 if you want python doing speech automatically for you
 from translate import Translator #Warning: Translation has limitation
 # get info using pyreptile
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
